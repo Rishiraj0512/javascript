94c7c7ceec162e671a802a76769b083235874d6e
Rishiraj0512 <rishiraj.23b1531153@abes.ac.in>
2026-08-17 21:24:10 +0530

    ok

--- 02_datatpye.js
+++ 02_datatpye.js
@@ -1,2 +1,3 @@
 //alert(3+3) we using nodejs, not browser
 
+console.log(typeof null)
---
+++ 03_conversion.js
@@ -0,0 +1,9 @@
+let score = "55"
+
+console.log(typeof score)
+
+let valueint = Number(score)
+
+console.log(typeof valueint)
+
+console.log(valueint)
